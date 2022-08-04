da0e033543dec2423772ee504c8937f68cc178c0
Jared Haynes <jared.haynes@charter.net>
2022-08-04 16:23:23 -0500

    Had to add = to the for loop to add the max value.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -7,7 +7,7 @@ const sumAll = function (min, max) {
         max = temp;
     }
     let sum = 0;
-    for (let i = min; i < max; i++) {
+    for (let i = min; i <= max; i++) {
         sum += i;
     }
     return sum;
