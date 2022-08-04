d1adf235790e80e6693763ec4a11ea23e85e13c9
Jared Haynes <jared.haynes@charter.net>
2022-08-03 19:56:21 -0500

    I looked up several solutions online for help. I still couldn't figure it out. I don't know if I can do this. It feels pretty hopeless right now.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function (word, times) {
+    let myString = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        myString += word;
+    }
+    return myString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
